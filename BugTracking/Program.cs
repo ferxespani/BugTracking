d3d1d9d506bf8621ec8bd7b683d06c689ab8c677
Vitaly <mickelangello45@gmail.com>
2018-12-10 19:59:59 +0200

    Create list tasks

--- BugTracking/Program.cs
+++ BugTracking/Program.cs
@@ -10,6 +10,17 @@ namespace BugTracking
     {
         static void Main(string[] args)
         {
+            List<Task> tasks = new List<Task>();
+            Random r = new Random();
+            int choice = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                choice = r.Next(1, 4);
+                if(choice == 1)
+                {
+                    
+                }
+            }
         }
     }
 }
